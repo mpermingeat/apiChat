ed7004fd353f3c93edb3430dc2bfa2d3b6e8052c
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-29 10:53:51 -0300

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
+import { CORS } from './config/cors';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
+
+  app.enableCors(CORS);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  ); //con este global pipe puedo utilizar los dto a traves del ValidationPipe
+
+  const configService = app.get(ConfigService); //para usar variable de entorno
+
+  await app.listen(+configService.get('PORT') || 3000);
+  console.log(`Api running on: localhost:3000`);
+}
+bootstrap();
